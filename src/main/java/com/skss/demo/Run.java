1d65f9084a6085740e0a8123de99370553f4dff0
chris <chrislon8850@gmail.com>
2018-01-15 18:22:55 +0800

    change

--- src/main/java/com/skss/demo/Run.java
+++ src/main/java/com/skss/demo/Run.java
@@ -1,6 +1,7 @@
 package com.skss.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
