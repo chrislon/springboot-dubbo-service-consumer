5a78a9f880bdea8933f74e0d3ddf6f55466faf0c
chris <chrislon8850@163.com>
2018-01-16 16:01:13 +0800

    去掉冲突包

--- src/main/java/com/skss/demo/Run.java
+++ src/main/java/com/skss/demo/Run.java
@@ -2,6 +2,8 @@ package com.skss.demo;
 
 import org.springframework.boot.SpringApplication;
 
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
